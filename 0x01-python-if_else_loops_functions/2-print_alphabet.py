390a4a19c1e38775a84dc6342f02584cd58bedcb
Odogwu <valentineodogwu@gmail.com>
2022-12-02 17:30:55 +0000

    task 1

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for c in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr((c)), end=""))
+res = ""
+for idx in range(97,123):
+       res = res + chr(idx)
+print(str(res))
