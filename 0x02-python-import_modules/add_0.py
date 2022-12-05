b14af45b4b46a2c8ac1445a8574d809141bb7063
Odogwu <valentineodogwu@gmail.com>
2022-12-05 11:45:36 +0000

    ts 1

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+    Args:
+	a: first integer
+	b: Second integer
+
+    Returns:
+	The return value: a + b
+    """
+    return (a + b)
